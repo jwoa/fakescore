54eb27d95c6140887d8d430b777a0b1065937722
Jason Agyekum <jason@seasonsix.co>
2022-12-04 18:08:50 +0000

    Push backup branch for Nyantea

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
 module.exports = {
     resolve: {
         // fallback: { 
@@ -7,15 +9,18 @@ module.exports = {
             "os": require.resolve("os-browserify/browser"),
             "https": require.resolve("https-browserify"),
             "fs": false,
-    "tls": false,
-    "net": false,
-    "path": false,
-    "zlib": false,
-    // "http": false,
-    "https": false,
-    "stream": false,
-    "crypto": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            // "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
         },
+        plugins: [
+            new NodePolyfillPlugin()
+        ]
     },
     node: {
         fs: "empty"
